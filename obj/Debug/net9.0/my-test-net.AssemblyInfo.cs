3ae9fcbbab7a71c2428448a0b0c1ace9b737c1a7
bimaassist15 <49235873+bimaega15@users.noreply.github.com>
2025-03-23 14:26:49 +0700

    Add Gitignore

--- obj/Debug/net9.0/my-test-net.AssemblyInfo.cs
+++ obj/Debug/net9.0/my-test-net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-test-net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e0ef68457a5a40b43b0e211967329bf872ad2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933b879fc3d60f13496b5cddd4e8d84b5865f409")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-test-net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-test-net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
